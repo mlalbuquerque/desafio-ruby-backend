32822005f45a5516e454f9e78548f34bbce9488b
thiaguerd <mail@thiago.pro>
2020-08-19 06:48:07 -0500

    add methods current_user current_user_authenticated and url_in to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def current_user
+    Usuario.find(session[:user]) if session[:user]
+  rescue StandardError
+    session.clear
+    nil
+  end
+
+  def current_user_authenticated
+    redirect_to entrar_path unless current_user
+  end
+
+  def url_in
+    Api::Google.url_in(request)
+  end
 end
