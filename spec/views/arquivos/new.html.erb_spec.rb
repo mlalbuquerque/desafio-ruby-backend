4f029e74626f6b949e7455a6c33c8edab584ac1e
thiaguerd <mail@thiago.pro>
2020-08-20 02:59:29 -0500

    write test view arquivos/new

--- spec/views/arquivos/new.html.erb_spec.rb
+++ spec/views/arquivos/new.html.erb_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'arquivos/new', type: :view do
   before do
-    assign(:arquivo, Arquivo.new(
-                       sha1: 'MyString'
-                     ))
+    assign(:arquivo, Arquivo.new())
   end
 
   it 'renders new arquivo form' do
     render
 
     assert_select 'form[action=?][method=?]', arquivos_path, 'post' do
-      assert_select 'input[name=?]', 'arquivo[sha1]'
+      assert_select 'input[name=?]', 'arquivo[documento]'
     end
   end
 end
