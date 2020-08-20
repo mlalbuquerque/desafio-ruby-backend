cc713886ae4f9a81495ecd0f5a72076eb7a43f2d
thiaguerd <mail@thiago.pro>
2020-08-19 20:06:58 -0500

    add uniqueness validation for sha1 on Arquivo

--- app/models/arquivo.rb
+++ app/models/arquivo.rb
@@ -1,5 +1,8 @@
 class Arquivo < ApplicationRecord
   has_one_attached :documento
+
+  validates :sha1, uniqueness: true
+
   def sha1_from_tempfile(file)
     self.sha1 = Digest::SHA1.file(file.path)
     self
