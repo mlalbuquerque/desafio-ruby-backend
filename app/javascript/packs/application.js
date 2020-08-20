35e4df036468a0bff032de31f3c0b82e68fe467f
thiaguerd <mail@thiago.pro>
2020-08-20 13:25:34 -0500

    install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
+import 'css/theme'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
