40dcc43a01e531a6b342b47066e11a3bff7524db
thiaguerd <mail@thiago.pro>
2020-08-20 01:43:41 -0500

    validate presence of sha1 on Arquivo

--- app/models/arquivo.rb
+++ app/models/arquivo.rb
@@ -2,6 +2,7 @@ class Arquivo < ApplicationRecord
   has_one_attached :documento
 
   validates :sha1, uniqueness: true
+  validates :sha1, presence: true
 
   def sha1_from_tempfile(file)
     self.sha1 = Digest::SHA1.file(file.path)
