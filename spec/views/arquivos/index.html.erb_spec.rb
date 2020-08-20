f08c5d4079bb639d1928b9118e22a77e267222c8
thiaguerd <mail@thiago.pro>
2020-08-20 02:59:11 -0500

    write tests view arquivos/index

--- spec/views/arquivos/index.html.erb_spec.rb
+++ spec/views/arquivos/index.html.erb_spec.rb
@@ -1,19 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'arquivos/index', type: :view do
-  before do
-    assign(:arquivos, [
-             Arquivo.create!(
-               sha1: 'Sha1'
-             ),
-             Arquivo.create!(
-               sha1: 'Sha1'
-             )
-           ])
+  let(:first_file) do
+    path = Rails.root.join('tmp/f1.txt')
+    File.open(path, 'w') { |f| f.write Time.now.to_f }
+    File.new(path)
   end
+
+  let(:first_arquive) do
+    arquivo = build(:arquivo)
+    arquivo.documento.attach(io: first_file, filename: File.basename(first_file))
+    arquivo.sha1_from_tempfile(first_file)
+    arquivo.save
+    arquivo
+  end
+
+  let(:second_file) do
+    path = Rails.root.join('tmp/f2.txt')
+    File.open(path, 'w') { |f| 3.times { f.write(Time.now.to_f) } }
+    File.new(path)
+  end
+
+  let(:second_arquive) do
+    arquivo = build(:arquivo)
+    arquivo.documento.attach(io: second_file, filename: File.basename(second_file))
+    arquivo.sha1_from_tempfile(second_file)
+    arquivo.save
+    arquivo
+  end
+
+  before { assign(:arquivos, [first_arquive, second_arquive]) }
 
   it 'renders a list of arquivos' do
     render
-    assert_select 'tr>td', text: 'Sha1'.to_s, count: 2
+    [first_arquive, second_arquive].each do
+      assert_select 'tr>td', text: first_arquive.sha1, count: 1
+    end
   end
 end
