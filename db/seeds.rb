6eadf9fd0f5771e06cf5a269c7b157adffdfc54d
thiaguerd <mail@thiago.pro>
2020-08-20 06:31:16 -0500

    add seed TipoDeTransacao

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+TipoDeTransacao.create(tipo: 1, descricao: 'Débito', natureza: 'Entrada', sinal: true)
+TipoDeTransacao.create(tipo: 2, descricao: 'Boleto', natureza: 'Saída', sinal: false)
+TipoDeTransacao.create(tipo: 3, descricao: 'Financiamento', natureza: 'Saída', sinal: false)
+TipoDeTransacao.create(tipo: 4, descricao: 'Crédito', natureza: 'Entrada', sinal: true)
+TipoDeTransacao.create(tipo: 5, descricao: 'Recebimento Empréstimo', natureza: 'Entrada', sinal: true)
+TipoDeTransacao.create(tipo: 6, descricao: 'Vendas', natureza: 'Entrada', sinal: true)
+TipoDeTransacao.create(tipo: 7, descricao: 'Recebimento TED', natureza: 'Entrada', sinal: true)
+TipoDeTransacao.create(tipo: 8, descricao: 'Recebimento DOC', natureza: 'Entrada', sinal: true)
+TipoDeTransacao.create(tipo: 9, descricao: 'Aluguel', natureza: 'Saída', sinal: false)
